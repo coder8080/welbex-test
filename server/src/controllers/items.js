d7a5982444d539559dc6f0d3a058e4853432b46a
Roman Kolerov <coder8080@mail.ru>
2022-07-27 14:39:00 +0300

    Using Limit & Offset for pagination

--- server/src/controllers/items.js
+++ server/src/controllers/items.js
@@ -1,22 +1,13 @@
 const client = require('../helpers/dbClient.js')
 
 module.exports.getItems = async (req, res) => {
-  const { maxvalue: maxValue, maxid: maxId, sorttype: sortType } = req.query
-  if (!['title', 'count', 'distance'].includes(sortType)) {
-    res.status(401).json({ error: 'Не указан порядок сортировки' })
-    return
-  }
-  let query
-  if (!maxValue || !maxId) {
-    query = `SELECT * FROM items ORDER BY ${sortType} ASC, id ASC LIMIT 10;`
-  } else if (maxValue && maxId) {
-    query = `SELECT * FROM items WHERE (${sortType}, id) > ('${maxValue}', ${maxId}) ORDER BY ${sortType} ASC, id ASC LIMIT 10;`
-  } else {
-    res.status(401).json({ error: 'Некорректные данные о последнем пункте' })
-    return
-  }
+  const { page, sorttype: sortType } = req.query
   try {
-    const result = await client.query(query)
+    const result = await client.query(
+      `SELECT * FROM items ORDER BY ${sortType} ASC LIMIT 10 OFFSET ${
+        (Number(page) - 1) * 10
+      };`
+    )
     res.status(200).json({ items: result.rows })
   } catch (error) {
     res.status(500).json({ error })
